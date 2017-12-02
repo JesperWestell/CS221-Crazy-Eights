501c83d29ab8756745382d66be6c0391cd28b4fb
Jesper Westell <jesper.westell@gmail.com>
2017-12-01 16:34:43 -0800

    Add basic run script run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+import evaluate
+import timeit
+
+games = 5
+#agents = 'MinimaxAgent,BaselineAgent'
+#agents = 'MinimaxAgent,OracleAgent'
+#agents = 'BasicMinimaxAgent,OracleAgent,BaselineAgent'
+#agents = 'MinimaxAgent,MinimaxAgent,BaselineAgent'
+#agents = 'RLAgent,BasicMinimaxAgent,BaselineAgent'
+agents = 'RLAgent,OracleAgent'
+verbose = 2
+fixseed = 'False'
+params = '-n %s -a %s -v %s -f %s' % (games,agents,verbose,fixseed)
+start = timeit.default_timer()
+evaluate.runGames(**evaluate.readCommand(params.split(' ')))
+end = timeit.default_timer()
+print
+print('Time per game: {0}'.format((end - start)/games))
+
